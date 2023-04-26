bab72151319bc29324b70bb9750f238884d8801d
CasianLW <ciorbcasian@gmail.com>
2023-04-26 19:30:52 +0200

    init 2nd essay TS -> JS

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <ion-app>
+    <ion-router-outlet />
+  </ion-app>
+</template>
+
+<script setup>
+import { IonApp, IonRouterOutlet } from "@ionic/vue";
+</script>
+
+<style>
+/* Couleurs personnalisées */
+:root {
+  --ion-color-primary: #d61e00;
+  --ion-color-primary-contrast: #ffffff;
+
+  --ion-color-secondary: #490900;
+  --ion-color-secondary-contrast: #ffffff;
+
+  --ion-color-tertiary: #3507004a;
+  --ion-color-tertiary-contrast: #ffffff;
+
+  --ion-color-quaternary: #4ea5d9;
+  --ion-color-quaternary-contrast: #ffffff;
+
+  --ion-color-quinary: #44cfcb;
+  --ion-color-quinary-contrast: #ffffff;
+
+  /* Autres styles */
+  --ion-toolbar-background: var(--ion-color-primary);
+  --ion-toolbar-color: var(--ion-color-primary-contrast);
+  --ion-background-color: var(--ion-color-secondary);
+  --ion-text-color: var(--ion-color-primary-contrast);
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,59 @@
+<template>
+    <ion-page>
+        <header-nav></header-nav>
+        <ion-content class="ion-content">
+                <ion-card>
+                    <iframe src="https://giphy.com/embed/vp2V5Rf8t4jHpx1bKk" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+                    <ion-card-header>
+                        <ion-card-title>FootApp</ion-card-title>
+                    </ion-card-header>
+                </ion-card>
+        </ion-content>
+        <footer-nav></footer-nav>
+    </ion-page>
+</template>
+
+<script>
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonCard, IonCardHeader, IonCardTitle } from '@ionic/vue';
+import HeaderNav from "@/components/HeaderNav.vue";
+import FooterNav from "@/components/FooterNav.vue";
+export default {
+    name: 'Home',
+    components: {
+        FooterNav,
+        HeaderNav,
+        IonPage,
+        IonHeader,
+        IonToolbar,
+        IonTitle,
+        IonContent,
+        IonList,
+        IonItem,
+        IonLabel,
+        IonCard,
+        IonCardHeader,
+        IonCardTitle,
+    },
+    data() {
+        return {
+            leagues: [
+                { id: 4334, name: 'Ligue 1' },
+                { id: 4401, name: 'Ligue 2' },
+                { id: 4637, name: 'National' },
+            ],
+        };
+    },
+    methods: {
+        openLeague(leagueId) {
+            this.$router.push({ name: 'LeagueTable', params: { id: leagueId } });
+        },
+    },
+};
+</script>
+
+<style scoped>
+.ion-content {
+    --padding-top: 50px;
+    --padding-bottom: 50px;
+}
+</style>
